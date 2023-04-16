35981aeab00138800ebf1b4473f0a3a464ed55ab
Albert <albert990624@gmail.com>
2023-04-16 18:44:49 +0800

    feat(fzf_edit): refactor code of fzf_edit.sh

--- my_scripts/script_fzf/fzf_edit.sh
+++ my_scripts/script_fzf/fzf_edit.sh
@@ -1,31 +1,6 @@
 function ff {
-  local target_dir="/Users/albert/CodeSpace/"
-  local ignore_dirs=(
-  "node_modules"
-  ".git"
-  "dist"
-  ".cache"
-  "voice-print"
-  "lodash"
-  "from_github"
-  "assets"
-  "image"
-  "images"
-  "static"
-  "data"
-  "*.jpg"
-  "*.png"
-  "*.svg"
-)
-
-  # 将 ignore_dirs 数组转化为 fd 的 --exclude 参数
-  local exclude_args=()
-  for dir in "${ignore_dirs[@]}"; do
-    exclude_args+=("--exclude" "${dir}")
-  done
-
-  # 使用 fd 命令搜索文件，并传递 --hidden 参数来搜索隐藏文件
-  local target_file=$(fd --type f --hidden "${exclude_args[@]}" --search-path "${target_dir}" | fzf --query="${target_dir} $1 $2")
-  local father_dir=$(dirname "${target_file}")
-  cd ${father_dir} && nvim ${target_file}
+    # 使用 fd 命令搜索文件，并传递 --hidden 参数来搜索隐藏文件
+    local target_file="$(fs $1 $2)"
+    local father_dir=$(dirname "${target_file}")
+    cd ${father_dir} && nvim ${target_file}
 }
