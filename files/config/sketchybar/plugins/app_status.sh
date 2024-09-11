bf7e0fb96f3bb29ad2c565656a794c7ba206ccf7
Albert <albert990624@gmail.com>
2024-09-11 21:55:11 +0800

    update

--- files/config/sketchybar/plugins/app_status.sh
+++ files/config/sketchybar/plugins/app_status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+app_padding=8
 if lsappinfo -all list | grep $NAME >>/dev/null; then
 	LABEL=$(lsappinfo -all list | grep $NAME | egrep -o "\"StatusLabel\"=\{ \"label\"=\"?(.*?)\"? \}" | sed 's/\"StatusLabel\"={ \"label\"=\(.*\) }/\1/g')
 	if [[ $LABEL =~ ^\".*\"$ ]]; then
@@ -7,13 +8,16 @@ if lsappinfo -all list | grep $NAME >>/dev/null; then
 		if [ -z "$LABEL" ]; then
 			# LABEL=0
 			LABEL=""
+			app_padding=0
 		fi
 	else
-		# LABEL=0
+		LABEL=""
+		app_padding=2
 		# LABEL=0
 	fi
 else
 	LABEL="?"
 fi
 
-sketchybar --set $NAME label=$LABEL
+sketchybar --set $NAME label=$LABEL \
+	label.padding_right=${app_padding}
